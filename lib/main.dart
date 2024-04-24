4a5e160f4d2ecd42c40de2c354531d3ebfbcd120
c-candycane <huseyinozcan5507@hotmail.com>
2024-04-24 11:22:01 +0300

    App ThemeData edits.

--- lib/main.dart
+++ lib/main.dart
@@ -7,30 +7,75 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+        colorScheme: const ColorScheme(
+            brightness: Brightness.dark,
+            primary: Colors.white,
+            onPrimary: Color.fromRGBO(31, 44, 55, 1),
+            secondary: Colors.blue,
+            onSecondary: Colors.white,
+            error: Colors.red,
+            onError: Colors.red,
+            background: Color.fromRGBO(31, 44, 55, 1),
+            onBackground: Color.fromRGBO(31, 44, 55, 1),
+            surface: Colors.white,
+            onSurface: Colors.white),
+
+          scaffoldBackgroundColor: Color.fromRGBO(31, 44, 55, 1),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Color.fromRGBO(31, 44, 55, 1),
+              titleTextStyle: TextStyle(color: Colors.white, fontSize: 24)),
+          progressIndicatorTheme:
+              const ProgressIndicatorThemeData(color: Colors.blue),
+          buttonTheme: const ButtonThemeData(buttonColor: Colors.blue),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          )),
+          switchTheme: SwitchThemeData(
+            thumbColor: MaterialStateProperty.all<Color>(Colors.blue),
+            trackColor: MaterialStateProperty.all<Color>(Colors.white),
+            trackOutlineColor:
+                MaterialStateProperty.all<Color>(Colors.transparent),
+          ),
+          checkboxTheme: CheckboxThemeData(
+            fillColor: MaterialStateProperty.all<Color>(Colors.white),
+            checkColor: MaterialStateProperty.all<Color>(Colors.blue),
+            overlayColor: MaterialStateProperty.all<Color>(Colors.blue),
+          ),
+          dropdownMenuTheme: const DropdownMenuThemeData(
+            textStyle: TextStyle(color: Colors.white),
+          ),
+          primaryColor: Colors.blue,
+          primarySwatch: Colors.blue,
+          indicatorColor: Colors.blue,
+          primaryTextTheme: const TextTheme(
+            bodyMedium: TextStyle(color: Colors.white),
+            displayMedium: TextStyle(color: Colors.white),
+            labelMedium: TextStyle(color: Colors.white),
+            titleMedium: TextStyle(color: Colors.white),
+            bodySmall: TextStyle(color: Colors.white),
+            displaySmall: TextStyle(color: Colors.white),
+            labelSmall: TextStyle(color: Colors.white),
+            titleSmall: TextStyle(color: Colors.white),
+          ),
+          listTileTheme: const ListTileThemeData(
+            textColor: Colors.white,
+          ),
+          iconButtonTheme: IconButtonThemeData(
+              style: ButtonStyle(
+            iconColor: MaterialStateProperty.all<Color>(Colors.blue),
+          )),
+          floatingActionButtonTheme: const FloatingActionButtonThemeData(
+            backgroundColor: Colors.blue,
+          )),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -39,14 +84,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -59,50 +96,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -119,7 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
